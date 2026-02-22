f9510caf099348993838c39d762cdeca16fc6ef7
muni-ramachandiran <142979658+MUNIRAMACHANDIRAN@users.noreply.github.com>
2026-02-22 07:55:55 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://your-frontend-name.onrender.com'
+        'https://portfolio-backend-q017.onrender.com'
     ],
     credentials: true
 }));
