9580e9e4d6870b4a44fe06530f1b37483f47b74c
muni-ramachandiran <142979658+MUNIRAMACHANDIRAN@users.noreply.github.com>
2026-02-22 07:38:42 +0530

    ok

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 // Attach JWT token to every request
-API.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem('portfolio_token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
